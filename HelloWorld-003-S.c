068de7e52ce8b547a0a81f344cff1d4de2414291
Türker Kızılcık <turkerkizilcik@gmail.com>
2023-10-07 23:29:59 +0300

    first c program

---
+++ HelloWorld-003-S.c
@@ -0,0 +1,115 @@
+/*
+ ============================================================================
+ Name        : HelloWorld-003-S.c
+ Author      : Turker Kizilcik
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//int main'deki int return'un çıktısını veriyor.
+// void ise input'taki "doesnt recieve any information"
+// printf teki f=formatted
+//
+
+int main(void) {
+	//printf("Hello World");
+	//printf("Welcome");
+	//printf(" to C!\n");
+	//printf("Welcome\nto\nC!\n");
+
+	/*
+	int integer1;
+	int integer2;
+
+	printf("Enter first integer\n");
+	scanf("%d", &integer1);
+
+	printf("Enter second integer\n");
+	scanf("%d", &integer2);
+
+	int sum;
+	sum = integer1 + integer2;
+	// veya int sum = integer1 + integer2
+
+	printf("Sum is %d", sum);
+	// veya sum yerine integer1 + integer2
+	return 1;
+	*/
+
+	/*
+	printf("Enter two integers and i will tell you\n");
+	printf("the relationships they satisfy: ");
+
+	int num1;
+	int num2;
+
+	scanf("%d %d", &num1, &num2);
+
+	if (num1 == num2) {
+		printf("%d is equal to %d\n", num1, num2);
+	}
+
+	if (num1 != num2) {
+		printf("%d is not equal to %d\n", num1, num2);
+	}
+
+	if (num1 > num2) {
+		printf("%d is greater than %d\n", num1, num2);
+	}
+
+	if (num1 < num2) {
+		printf("%id is less than %d\n", num1, num2);
+	}
+
+	if (num1 <= num2) {
+		printf("%id is less or equal to %d\n", num1, num2);
+	}
+
+	if (num1 >= num2) {
+		printf("%id is greater or equal to %d\n", num1, num2);
+	}
+	*/
+
+	//printf("Welcome");
+	//printf("%s","Welcome");
+
+	/*
+	int c;
+	int thisVariable;
+	int q76354;
+	int a;
+
+	printf("Enter an integer: ");
+
+	scanf("%d", &a);
+
+	if (a != 7) {
+		printf("The variable number is not equal to 7\n");
+	}
+
+	printf("This is a C program.\n");
+	printf("This is a C\n");
+	printf("program.\n");
+	printf("This\nis\na\nC\nprogram.\n");
+	printf("This\tis\ta\tC\tprogram.\t");
+
+	*/
+
+    /*
+	int x, y, z;
+	printf("Type 3 integers: ");
+	scanf("%d %d %d", &x, &y, &z);
+
+	int result = x * y * z;
+
+	printf("The result is %d", result);
+ 
+    */
+
+}
